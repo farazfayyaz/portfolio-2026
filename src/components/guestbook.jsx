1c41ef56556c0805eeb43341b212ad831e2fbc9f
Faraz Fayyaz <farazfayyazwork@gmail.com>
2026-06-22 19:45:46 -0400

    tailwind css update

--- src/components/guestbook.jsx
+++ src/components/guestbook.jsx
@@ -133,7 +133,7 @@ export default function Guestbook() {
         </div>
 
         {/* The Corkboard */}
-        <div className="lg:col-span-2 min-h-[500px] bg-[url('https://www.transparenttextures.com/patterns/cork-board.png')] bg-slate-800/40 border border-slate-800 rounded-3xl p-8 relative overflow-hidden flex flex-wrap content-start gap-4 shadow-inner">
+        <div className="lg:col-span-2 min-h-125 bg-[url('https://www.transparenttextures.com/patterns/cork-board.png')] bg-slate-800/40 border border-slate-800 rounded-3xl p-8 relative overflow-hidden flex flex-wrap content-start gap-4 shadow-inner">
           
           {/* Loading State */}
           {isLoading ? (
